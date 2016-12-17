c84ef9ffef37ef10f4a22806b7428dfd278809a3
Rick Clark <rkingsleyclark@gmail.com>
2016-12-17 11:46:17 +0000

    Second test added and code working

--- spec/greeter_spec.rb
+++ spec/greeter_spec.rb
@@ -1,7 +1,11 @@
-require '../lib/greeter.rb'
+require 'greeter'
 
 describe "Greeter" do
   it "greets Rico" do
     expect(greet("Rico")).to eq "Hello, Rico, how are you today?"
+  end
+
+  it "greets Chloe" do
+    expect(greet("Chloe")).to eq "Hello, Chloe, how are you today?"
   end
 end
